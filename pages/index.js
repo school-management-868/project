d51bc3e63c085af58ac1009351ddde9daae69994
akashgandhar <akashgandhar007@gmail.com>
2022-10-06 16:08:24 +0530

    3

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,18 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Slider from "../components/slider";
+import Content from "../components/content";
+import Feature from "../components/feature";
 
 
 export default function Home() {
-  return <Slider/>;
+  return (
+  <><Slider/>
+  <Feature/>
+  <Content/>
+  <div style={{padding:200}}></div>
+
+  
+  </>
+)
 }
