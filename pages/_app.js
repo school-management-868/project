15fc76d1a8efd8b98700d54eace14588c82b992e
akashgandhar <akashgandhar007@gmail.com>
2022-10-06 15:37:16 +0530

    1

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import Footer from "../components/footer";
+import Header from "../components/header";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
